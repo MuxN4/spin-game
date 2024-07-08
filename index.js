2fc9d2e0806a161a44091cb69c312b3b6b636b1c
Steve <stev3npy@gmail.com>
2024-07-08 11:42:38 +0300

    Restructured the number of lines function

--- index.js
+++ index.js
@@ -32,15 +32,16 @@ const deposit = () => {
     }
 };
 
+// Function to get the number of lines to bet on
 const getNumberOfLines = () => {
     while (true) {
         const lines = prompt("Enter the number of lines to bet on (1-3): ");
         const numberOfLines = parseFloat(lines);
 
         if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
-            console.log("Invalid number of lines,try again.")
-        }else{
-            return numberOfLines
+            console.log("Invalid number of lines, try again.");
+        } else {
+            return numberOfLines;
         }
     }
 };
